f8fc597677a87f1f670ebf5a6e70cf8a21f1283c
David Moreno <david@axiombox.com>
2009-08-24 02:01:39 -0400

    staging removable dirs

---
+++ _old_www/principal/index.php
@@ -0,0 +1,23 @@
+<?php require("header.php"); ?>
+<div id="ContTabul" align="center">
+	<p><img src="images/planetalinux-main.png">
+		<br /><br />
+		<div id="banderas" align="center">
+		<a href="http://ec.planetalinux.org/"><img src="images/banderas/ec.png"></a>&nbsp;&nbsp;
+		<a href="http://es.planetalinux.org/"><img src="images/banderas/es.png"></a>&nbsp;&nbsp;
+		<a href="http://cl.planetalinux.org/"><img src="images/banderas/cl.png"></a>&nbsp;&nbsp;
+		<a href="http://co.planetalinux.org/"><img src="images/banderas/co.png"></a>&nbsp;&nbsp;
+		<a href="http://sv.planetalinux.org/"><img src="images/banderas/sv.png"></a>&nbsp;&nbsp;
+		<a href="http://gt.planetalinux.org/"><img src="images/banderas/gt.png"></a>&nbsp;&nbsp;
+		<br />
+		<a href="http://mx.planetalinux.org/"><img src="images/banderas/mx.png"></a>&nbsp;&nbsp;
+		<a href="http://ni.planetalinux.org/"><img src="images/banderas/ni.png"></a>&nbsp;&nbsp;
+		<a href="http://pe.planetalinux.org/"><img src="images/banderas/pe.png"></a>&nbsp;&nbsp;
+		<a href="http://ve.planetalinux.org/"><img src="images/banderas/ve.png"></a>&nbsp;&nbsp;
+		<a href="http://pa.planetalinux.org/"><img src="images/banderas/pa.png"></a>
+		</div>
+	</p>
+</div>
+<?php require("footer.php"); ?>
+</body>
+</html>
---
+++ _sandbox/hola.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+#!/usr/bin/env python
+#:p
+
+# Este es un comentario :-)
+die or die;
+use warnings;
+#use fuse;
+#use suse;
+#use xuse;
+
+open(PY, "/bin/the_real_program.py");
+# sí
+
+# :-S
+# wow :-)
+#peque ya toy aqui;
+#lestat was here
+print "Hallo Welt!";
+
+__END__
+
+github?
+ñeñeñe
