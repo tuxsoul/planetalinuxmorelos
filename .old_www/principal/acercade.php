59c1309052eedcc5f4a0879b3802395055fe5109
Mario Oyorzabal Salgado <tuxsoul@gmail.com>
2016-04-13 02:40:19 +0000

    remove old_www folder ...
    Signed-off-by: Mario Oyorzabal Salgado <tuxsoul@gmail.com>

--- .old_www/principal/acercade.php
+++
@@ -1,9 +0,0 @@
-<?php require("header.php"); ?>
-<div id="contenido">
-<titulo>Acerca de</titulo>
-	<p>Planeta Linux es Planeta Linux.</p>
-</div>
-<?php require("footer.php"); ?>
-</body>
-</html>
-
